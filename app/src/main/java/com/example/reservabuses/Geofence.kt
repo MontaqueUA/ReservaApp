6518880dcce2357170484bdde782ec8ac00c85f5
unknown <iacarril@miaundes.cl>
2019-08-02 17:15:20 -0400

    final

--- app/src/main/java/com/example/reservabuses/Geofence.kt
+++
@@ -1,9 +0,0 @@
-package com.example.reservabuses
-
-import android.content.Context
-import com.google.android.gms.location.LocationServices
-
-class Geofence private constructor(context: Context) {
-    private val geofencingClient = LocationServices.getGeofencingClient(context)
-
-}
